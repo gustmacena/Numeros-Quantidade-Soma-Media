6dbea8b91e95bc628f90850fc536cd0f94ac3a65
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 20:32:06 -0300

    Numeros Python

---
+++ numeros.py
@@ -0,0 +1,15 @@
+# 4 - Escreva um programa que leia números inteiros do teclado.
+# O programa deve ler os números até que o usuário digite 0 (zero).
+# No final da execução, exiba a quantidade de números digitados não
+# levando em consideração o zero, assim como a soma e a média aritmética.
+s = 0
+q = 0
+while True:
+    n = int(input("Digite um número inteiro:"))
+    if n == 0:
+        break
+    s = s + n
+    q = q + 1
+print(f"Quantidade de números digitados: {q}")
+print(f"Soma: {s}")
+print(f"Média: {s / q:.2f}")
